8bfda5f4e9a8226dd6c16aaf97f3d968560648f1
Kristian Perez <kpere192@fiu.edu>
2024-04-04 13:53:59 -0400

    Added color for grid, added mouse over event that turns grid black when hovering over

--- javascript.js
+++ javascript.js
@@ -10,15 +10,26 @@ function makeGrid()
   }
 }
 
+// Makes a single plot of grid
 function makePlot()
 {
   const gridPlot = document.createElement("div");
   const sides = 100 / DIMENSION;
   gridPlot.style.width = sides + "%";
   gridPlot.style.height = sides + "vh";
-  gridPlot.style.border = "1px solid black";
+  gridPlot.style.border = "1px solid rgb(161, 164, 190)";
+  gridPlot.style.backgroundColor = "rgb(180, 164, 161)";
+
+  gridPlot.addEventListener("mouseenter", (event) => {
+    plotHovered(event)
+  });
+
   container.appendChild(gridPlot);
 }
 
+function plotHovered(e)
+{
+  e.target.style.backgroundColor = "black";
+}
 
 makeGrid();
